f131afd635740704946c1bdbdf1fe17ef1b4ea74
MonzUn <daniel-bengtsson@live.com>
2018-01-02 17:04:49 +0100

    Updated MEngine (Image splitting and thread safety of screenshot functions)

--- include/MEngine/mengineGraphics.h
+++ include/MEngine/mengineGraphics.h
@@ -17,9 +17,10 @@ namespace MEngineGraphics
 		int32_t Height = -1;
 	};
 
-	typedef int64_t MEngineTextureID;
+	typedef int32_t MEngineTextureID;
 
 	void				UnloadTexture(MEngineTextureID textureID);
+	MEngineTextureID	CreateSubTextureFromTextureData(const MEngineTextureData& originalTexture, int32_t upperLeftOffsetX, int32_t upperLeftOffsetY, int32_t lowerRightOffsetX, int32_t lowerRightOffsetY, bool storeCopyInRAM = false);
 	MEngineTextureID	CreateTextureFromTextureData(const MEngineTextureData& textureData, bool storeCopyInRAM = false);
 	MEngineTextureID	CaptureScreenToTexture(bool storeCopyInRAM = false);
 
