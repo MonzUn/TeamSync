3fb465ebfa386281d5b003f36d9f05f959729cc9
MonzUn <daniel-bengtsson@live.com>
2018-03-20 10:30:05 +0100

    Added TODO

--- source/main.cpp
+++ source/main.cpp
@@ -11,11 +11,12 @@
 // TODODB: Rename source files using uppercase initial character
 // TODODB: Fix memory leak that occurs after a client exits the program while connected to a host
 // TODODB: Fix memory leak that occurs after a host exits the program before any clients connect
+// TODODB: Fix crash on shutdown in release only
 // TODODB: Make the application UI scalable
 // TODODB: Make clipping and destination rects customizable from file
 // TODODB: Make clipping/destination rect setups selectable from main menu
 // TODODB: Remove window border and make it possible to move the app between screens via UI/Console
-// TODODB: Create setting for making remote clients send thir log to the host  before they disconnect
+// TODODB: Create setting for making remote clients send thir log to the host before they disconnect
 
 #if PLATFORM == PLATFORM_WINDOWS
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
